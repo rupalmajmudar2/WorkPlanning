142d4cc37189814e9b1beeeae18052de4e4c6d35
Rupal Majmudar <rupal.majmudar@bob.ch>
2022-04-09 12:53:46 +0200

    update

---
+++ RmWorkPlanning/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RmWorkPlanningApp {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+        }
+        // Use this method to configure the HTTP request pipeline.  
+        public void Configure(IApplicationBuilder app) {
+        }
+    }
+}
---
+++ RmWorkPlanningNUnits/WorkPlanTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using RmWorkPlanning;
+
+using RmWorkPlanningApp;
+
+namespace RmWorkPlanningNUnits {
+    public class WorkPlanTests {
+        private WorkPlanController _workPlanController;
+
+        [SetUp]
+        public void Init() {
+            WorkPlanRepository workPlanRepo = new WorkPlanRepository();
+            WorkPlanService workPlanService = new WorkPlanService(workPlanRepo);
+            _workPlanController = new WorkPlanController(workPlanService);
+        }
+
+        [Test]
+        public void TestStart() {
+            //_workPlanController.
+        }
+
+        [TearDown]
+        public void Cleanup() { 
+            _workPlanController = null;
+        }
+    }
+}
